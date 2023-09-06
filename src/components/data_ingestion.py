bfa5215c35a7ee75b9b92f400e9fc291d2715e0e
VYBHAV97 <vybhav.S@powerschool.com>
2023-09-06 09:53:44 +0530

    Model trainer and hyperparameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 
 @dataclass
 #Without a __init__() constructor, the class accepts values and assigns it to appropriate variables.
@@ -36,10 +42,14 @@ class DataIngestion:
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
             logging.info('Ingestion of data is completed')
+            logging.info(self.ingestion_config.train_data_path)
+            logging.info(self.ingestion_config.test_data_path)
 
             return(
                 self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path
+                
+                self.ingestion_config.test_data_path,
+                
             )
         
         except Exception as e:
@@ -47,4 +57,10 @@ class DataIngestion:
         
 if __name__=='__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()                
+    train_data,test_data=obj.initiate_data_ingestion()            
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)    
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
